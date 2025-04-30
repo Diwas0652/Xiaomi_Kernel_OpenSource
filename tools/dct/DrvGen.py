e6c2d5de891beeeb4acba29404073c4a392cd325
Diwas Neupane <neupanediwas652@gmail.com>
2025-04-30 10:33:30 +0545

    Update(2) DrvGen.py

--- tools/dct/DrvGen.py
+++ tools/dct/DrvGen.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 MediaTek Inc.
@@ -10,9 +10,10 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See http://www.gnu.org/licenses/gpl-2.0.html for more details.	
+# See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 
-import os, sys
+import os
+import sys
 import getopt
 import traceback
 import subprocess
@@ -42,6 +43,7 @@ from obj.ChipObj import MT6779
 from utility.util import LogLevel
 from utility.util import log
 
+
 def usage():
     print('''
 usage: DrvGen [dws_path] [file_path] [log_path] [paras]...
@@ -54,33 +56,35 @@ log_path    :    where to store the log files
 paras        :    parameter for generate wanted file
 ''')
 
+
 def is_oldDws(path, gen_spec):
     if not os.path.exists(path):
-        log(LogLevel.error, 'Can not find %s' %(path))
+        log(LogLevel.error, 'Can not find %s' % (path))
         sys.exit(-1)
 
     try:
         root = xml.dom.minidom.parse(dws_path)
-    except Exception, e:
-        log(LogLevel.warn, '%s is not xml format, try to use old DCT!' %(dws_path))
+    except Exception as e:
+        log(LogLevel.warn, '%s is not xml format, try to use old DCT!' % (dws_path))
         if len(gen_spec) == 0:
             log(LogLevel.warn, 'Please use old DCT UI to gen all files!')
             return True
         old_dct = os.path.join(sys.path[0], 'old_dct', 'DrvGen')
         cmd = old_dct + ' ' + dws_path + ' ' + gen_path + ' ' + log_path + ' ' + gen_spec[0]
-        if 0 == subprocess.call(cmd, shell=True):
+        if subprocess.call(cmd, shell=True) == 0:
             return True
         else:
-            log(LogLevel.error, '%s format error!' %(dws_path))
+            log(LogLevel.error, '%s format error!' % (dws_path))
             sys.exit(-1)
 
     return False
+
 
 if __name__ == '__main__':
     opts, args = getopt.getopt(sys.argv[1:], '')
 
     if len(args) == 0:
-        msg = 'Too less arguments!'
+        msg = 'Too few arguments!'
         usage()
         log(LogLevel.error, msg)
         sys.exit(-1)
@@ -109,74 +113,69 @@ if __name__ == '__main__':
     elif len(args) >= 4:
         gen_path = os.path.abspath(args[1])
         log_path = os.path.abspath(args[2])
-        for i in range(3,len(args)):
+        for i in range(3, len(args)):
             gen_spec.append(args[i])
 
-    log(LogLevel.info, 'DWS file path is %s' %(dws_path))
-    log(LogLevel.info, 'Gen files path is %s' %(gen_path))
-    log(LogLevel.info, 'Log files path is %s' %(log_path))
+    log(LogLevel.info, 'DWS file path is %s' % (dws_path))
+    log(LogLevel.info, 'Gen files path is %s' % (gen_path))
+    log(LogLevel.info, 'Log files path is %s' % (log_path))
 
     for item in gen_spec:
-        log(LogLevel.info, 'Parameter is %s' %(item))
-
-
+        log(LogLevel.info, 'Parameter is %s' % (item))
 
     # check DWS file path
     if not os.path.exists(dws_path):
-        log(LogLevel.error, 'Can not find "%s", file not exist!' %(dws_path))
+        log(LogLevel.error, 'Can not find "%s", file not exist!' % (dws_path))
         sys.exit(-1)
 
     if not os.path.exists(gen_path):
-        log(LogLevel.error, 'Can not find "%s", gen path not exist!' %(gen_path))
+        log(LogLevel.error, 'Can not find "%s", gen path not exist!' % (gen_path))
         sys.exit(-1)
 
     if not os.path.exists(log_path):
-        log(LogLevel.error, 'Can not find "%s", log path not exist!' %(log_path))
+        log(LogLevel.error, 'Can not find "%s", log path not exist!' % (log_path))
         sys.exit(-1)
 
     if is_oldDws(dws_path, gen_spec):
         sys.exit(0)
 
     chipId = ChipObj.get_chipId(dws_path)
-    log(LogLevel.info, 'chip id: %s' %(chipId))
+    log(LogLevel.info, 'chip id: %s' % (chipId))
     chipObj = None
-    if cmp(chipId, 'MT6797') == 0:
+
+    if chipId == 'MT6797':
         chipObj = Everest(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757') == 0:
+    elif chipId == 'MT6757':
         chipObj = Olympus(dws_path, gen_path)
-    elif cmp(chipId, 'MT6757-P25') == 0:
+    elif chipId == 'MT6757-P25':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'KIBOPLUS') == 0:
+    elif chipId == 'KIBOPLUS':
         chipObj = MT6757_P25(dws_path, gen_path)
-    elif cmp(chipId, 'MT6570') == 0:
+    elif chipId == 'MT6570':
         chipObj = Rushmore(dws_path, gen_path)
-    elif cmp(chipId, 'MT6799') == 0:
+    elif chipId == 'MT6799':
         chipObj = Whitney(dws_path, gen_path)
-    elif cmp(chipId, 'MT6763') == 0:
+    elif chipId == 'MT6763':
         chipObj = MT6763(dws_path, gen_path)
-    elif cmp(chipId, 'MT6759') == 0:
+    elif chipId == 'MT6759':
         chipObj = MT6759(dws_path, gen_path)
-    elif cmp(chipId, 'MT6750S') == 0:
+    elif chipId == 'MT6750S':
         chipObj = MT6750S(dws_path, gen_path)
-    elif cmp(chipId, 'MT6758') == 0:
+    elif chipId == 'MT6758':
         chipObj = MT6758(dws_path, gen_path)
-    elif cmp(chipId, 'MT6739') == 0:
+    elif chipId == 'MT6739':
         chipObj = MT6739(dws_path, gen_path)
-    elif cmp(chipId, 'MT8695') == 0:
+    elif chipId == 'MT8695':
         chipObj = MT8695(dws_path, gen_path)
-    elif cmp(chipId, 'MT6771') == 0 or \
-         cmp(chipId, 'MT6775') == 0 or \
-         cmp(chipId, 'MT6765') == 0 or \
-         cmp(chipId, 'MT3967') == 0 or \
-         cmp(chipId, 'MT6761') == 0:
+    elif chipId in ['MT6771', 'MT6775', 'MT6765', 'MT3967', 'MT6761']:
         chipObj = MT6771(dws_path, gen_path)
-    elif cmp(chipId, 'MT6779') == 0:
+    elif chipId == 'MT6779':
         chipObj = MT6779(dws_path, gen_path)
     else:
         chipObj = ChipObj(dws_path, gen_path)
 
     if not chipObj.parse():
-        log(LogLevel.error, 'Parse %s fail!' %(dws_path))
+        log(LogLevel.error, 'Parse %s fail!' % (dws_path))
         sys.exit(-1)
 
     if not chipObj.generate(gen_spec):
@@ -184,4 +183,3 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     sys.exit(0)
-
